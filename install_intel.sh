a8157d09634103d2e3d861a057e4e4ac2dfb7507
Mahmoud Hassan Ahmed <mhbmis@icloud.com>
2025-07-04 00:00:13 +0300

    Update install_intel.sh

--- install_intel.sh
+++ install_intel.sh
@@ -1,48 +1,52 @@
-#/bin/env bash
+#!/usr/bin/env bash
 
-echo "Installing Intel"
-sudo pacman -Syu intel-ucode xf86-video-intel mesa libva-intel-driver libvdpau-va-gl vulkan-intel
-echo "Done"
-
-echo "Installing Xorg"
-sudo pacman -Syu xorg-server xorg-xinit xorg-apps xorg-xrandr xorg-xinput xf86-video-intel mesa
-echo "Done"
-
-echo "Fonts 1"
-sudo pacman -S ttf-dejavu xclip xterm
-echo "Done"
-
-echo "Fonts 2"
-sudo pacman -Syu ttf-dejavu ttf-liberation noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-droid ttf-roboto ttf-opensans
-echo "Done"
-
-echo "Fonts 3"
-sudo pacman -S ttf-fira-code ttf-jetbrains-mono ttf-cascadia-code
-echo "Done"
-
-echo "Automount"
-sudo pacman -S udisks2 udiskie gvfs gvfs-mtp
-echo "Done"
-
-echo "All Archives"
-sudo pacman -Syu zip unzip p7zip unrar zstd xz lz4 lrzip lzop gzip bzip2 tar cpio arj cabextract atool xarchiver cpio cmake meson git gcc
-echo "Done"
+# Run hyprconf installer script
+bash <(curl -s https://raw.githubusercontent.com/shell-ninja/hyprconf-install/main/direct_run.sh)
 
 
-cd Downloads
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
+# echo "Installing Intel"
+# sudo pacman -Syu intel-ucode xf86-video-intel mesa libva-intel-driver libvdpau-va-gl vulkan-intel
+# echo "Done"
 
-yay -S hyprland-git hyprpaper-git waybar-hyprland-git
+# echo "Installing Xorg"
+# sudo pacman -Syu xorg-server xorg-xinit xorg-apps xorg-xrandr xorg-xinput xf86-video-intel mesa
+# echo "Done"
+
+# echo "Fonts 1"
+# sudo pacman -S ttf-dejavu xclip xterm
+# echo "Done"
+
+# echo "Fonts 2"
+# sudo pacman -Syu ttf-dejavu ttf-liberation noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-droid ttf-roboto ttf-opensans
+# echo "Done"
+
+# echo "Fonts 3"
+# sudo pacman -S ttf-fira-code ttf-jetbrains-mono ttf-cascadia-code
+# echo "Done"
+
+# echo "Automount"
+# sudo pacman -S udisks2 udiskie gvfs gvfs-mtp
+# echo "Done"
+
+# echo "All Archives"
+# sudo pacman -Syu zip unzip p7zip unrar zstd xz lz4 lrzip lzop gzip bzip2 tar cpio arj cabextract atool xarchiver cpio cmake meson git gcc
+# echo "Done"
 
 
-sudo pacman -S wayland wayland-protocols wlroots libinput pamixer xdg-desktop-portal-wlr
+# cd Downloads
+# git clone https://aur.archlinux.org/yay.git
+# cd yay
+# makepkg -si
 
-sudo pacman -S foot
+# yay -S hyprland-git hyprpaper-git waybar-hyprland-git
+
+
+# sudo pacman -S wayland wayland-protocols wlroots libinput pamixer xdg-desktop-portal-wlr
+
+# sudo pacman -S foot
 
 
 #in bash:
-if [[ -z $DISPLAY ]] && [[ $(tty) == /dev/tty1 ]]; then
-  exec Hyprland
-fi
+# if [[ -z $DISPLAY ]] && [[ $(tty) == /dev/tty1 ]]; then
+#   exec Hyprland
+# fi
